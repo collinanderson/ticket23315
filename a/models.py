c813d1b7e8b46f86d0fb01e7d19bb69e80c8b76a
Collin Anderson <cmawebsite@gmail.com>
2014-08-19 12:17:52 -0400

    simple models

--- a/models.py
+++ a/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    country = models.ForeignKey('b.DeliveryCountry')
+
+class Person(models.Model):
+    pass
--- b/models.py
+++ b/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class APackage(models.Model):
+    delivery_person = models.ForeignKey('a.Person')
+
+class DeliveryCountry(models.Model):
+    pass
